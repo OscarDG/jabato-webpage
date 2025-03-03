8d60653fe9f6a34a8eacaa0bdf974295816aebc9
OscarDG <oscard.guerrerom@outlook.com>
2025-03-02 20:37:42 -0500

    Updated path for the banner image

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import { anton, inter } from "@/app/ui/fonts";
-import Hero from "./ui/Hero";
+import Hero from "@/app/ui/Hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <section className="flex justify-center items-center w-full h-80 bg-[url(@/public/pattern.png)] bg-black">
+      <section className="flex justify-center items-center w-full h-80 bg-[url(/pattern.png)] bg-black">
         <h1 className={`${anton.className} w-50 md:w-200 text-left md:text-center text-[3rem] md:text-[5rem] text-brandred`}>CERVEZAS CON CARACTER</h1>
       </section>
     </>
